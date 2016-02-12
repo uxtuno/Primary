927c4c2469f9dfc0ff707e283104f61d674bd4f6
uxtuno <uxtuno664@gmail.com>
2016-02-12 20:30:30 +0900

    とりあえずコミット

---
+++ Assets/Scripts/NotMonoBehaviour/SceneName.cs
@@ -0,0 +1,157 @@
+public class SceneName
+{
+
+	/// <summary>
+	/// return "Menu"
+ 	/// </summary>
+	public const string @Menu = "Menu";
+
+	/// <summary>
+	/// return "TutorialStage01"
+ 	/// </summary>
+	public const string @TutorialStage01 = "TutorialStage01";
+
+	/// <summary>
+	/// return "Stage02"
+ 	/// </summary>
+	public const string @Stage02 = "Stage02";
+
+	/// <summary>
+	/// return "TutorialStage02"
+ 	/// </summary>
+	public const string @TutorialStage02 = "TutorialStage02";
+
+	/// <summary>
+	/// return "UXStage01"
+ 	/// </summary>
+	public const string @UXStage01 = "UXStage01";
+
+	/// <summary>
+	/// return "UXStage02"
+ 	/// </summary>
+	public const string @UXStage02 = "UXStage02";
+
+	/// <summary>
+	/// return "Stage01"
+ 	/// </summary>
+	public const string @Stage01 = "Stage01";
+
+	/// <summary>
+	/// return "Stage03"
+ 	/// </summary>
+	public const string @Stage03 = "Stage03";
+
+	/// <summary>
+	/// return "Stage05"
+ 	/// </summary>
+	public const string @Stage05 = "Stage05";
+
+	/// <summary>
+	/// return "Stage04"
+ 	/// </summary>
+	public const string @Stage04 = "Stage04";
+
+	/// <summary>
+	/// return "UXTestScene"
+ 	/// </summary>
+	public const string @UXTestScene = "UXTestScene";
+
+	/// <summary>
+	/// return "K.R.EasyStage01"
+ 	/// </summary>
+	public const string @KREasyStage01 = "K.R.EasyStage01";
+
+	/// <summary>
+	/// return "K.R.EasyStage02"
+ 	/// </summary>
+	public const string @KREasyStage02 = "K.R.EasyStage02";
+
+	/// <summary>
+	/// return "K.R.EasyStage03"
+ 	/// </summary>
+	public const string @KREasyStage03 = "K.R.EasyStage03";
+
+	/// <summary>
+	/// return "K.R.EasyStage05"
+ 	/// </summary>
+	public const string @KREasyStage05 = "K.R.EasyStage05";
+
+	/// <summary>
+	/// return "K.R.MiddleStage01"
+ 	/// </summary>
+	public const string @KRMiddleStage01 = "K.R.MiddleStage01";
+
+	/// <summary>
+	/// return "K.R.MiddleStage02"
+ 	/// </summary>
+	public const string @KRMiddleStage02 = "K.R.MiddleStage02";
+
+	/// <summary>
+	/// return "K.R.MiddleStage03"
+ 	/// </summary>
+	public const string @KRMiddleStage03 = "K.R.MiddleStage03";
+
+	/// <summary>
+	/// return "K.R.HardStage01"
+ 	/// </summary>
+	public const string @KRHardStage01 = "K.R.HardStage01";
+
+	/// <summary>
+	/// return "N.EasyStage01"
+ 	/// </summary>
+	public const string @NEasyStage01 = "N.EasyStage01";
+
+	/// <summary>
+	/// return "N.EasyStage02"
+ 	/// </summary>
+	public const string @NEasyStage02 = "N.EasyStage02";
+
+	/// <summary>
+	/// return "N.EasyStage03"
+ 	/// </summary>
+	public const string @NEasyStage03 = "N.EasyStage03";
+
+	/// <summary>
+	/// return "N.HardStage01"
+ 	/// </summary>
+	public const string @NHardStage01 = "N.HardStage01";
+
+	/// <summary>
+	/// return "EndScene"
+ 	/// </summary>
+	public const string @EndScene = "EndScene";
+
+	/// <summary>
+	/// return "N.HardStage02"
+ 	/// </summary>
+	public const string @NHardStage02 = "N.HardStage02";
+
+	/// <summary>
+	/// <para>0. "Menu"</para>
+	/// <para>1. "TutorialStage01"</para>
+	/// <para>2. "Stage02"</para>
+	/// <para>3. "TutorialStage02"</para>
+	/// <para>4. "UXStage01"</para>
+	/// <para>5. "UXStage02"</para>
+	/// <para>6. "Stage01"</para>
+	/// <para>7. "Stage03"</para>
+	/// <para>8. "Stage05"</para>
+	/// <para>9. "Stage04"</para>
+	/// <para>10. "UXTestScene"</para>
+	/// <para>11. "K.R.EasyStage01"</para>
+	/// <para>12. "K.R.EasyStage02"</para>
+	/// <para>13. "K.R.EasyStage03"</para>
+	/// <para>14. "K.R.EasyStage05"</para>
+	/// <para>15. "K.R.MiddleStage01"</para>
+	/// <para>16. "K.R.MiddleStage02"</para>
+	/// <para>17. "K.R.MiddleStage03"</para>
+	/// <para>18. "K.R.HardStage01"</para>
+	/// <para>19. "N.EasyStage01"</para>
+	/// <para>20. "N.EasyStage02"</para>
+	/// <para>21. "N.EasyStage03"</para>
+	/// <para>22. "N.HardStage01"</para>
+	/// <para>23. "EndScene"</para>
+	/// <para>24. "N.HardStage02"</para>
+	/// </summary>
+	public static readonly string[] names = new string[]{"Menu","TutorialStage01","Stage02","TutorialStage02","UXStage01","UXStage02","Stage01","Stage03","Stage05","Stage04","UXTestScene","K.R.EasyStage01","K.R.EasyStage02","K.R.EasyStage03","K.R.EasyStage05","K.R.MiddleStage01","K.R.MiddleStage02","K.R.MiddleStage03","K.R.HardStage01","N.EasyStage01","N.EasyStage02","N.EasyStage03","N.HardStage01","EndScene","N.HardStage02"};
+}
