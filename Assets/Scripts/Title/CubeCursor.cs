c314a8ac23db27d89e4b19c4cadd097915020036
Kuvo <Kuvo.Do.Laurant.Ehrengerg@gmail.com>
2016-02-14 06:33:43 +0900

    Menuシーンを調整

--- Assets/Scripts/Title/CubeCursor.cs
+++ Assets/Scripts/Title/CubeCursor.cs
@@ -3,17 +3,16 @@ using System.Collections;
 
 public class CubeCursor : MyMonoBehaviour
 {
-    [SerializeField]
-    private Vector3 axis = Vector3.zero;
+	[SerializeField]
+	private Vector3 axis = Vector3.zero;
 
-    protected override void Awake()
-    {
-        base.Awake();
-    }
+	protected override void Awake()
+	{
+		base.Awake();
+	}
 
-    protected override void Update()
-    {
-        base.Update();
-        transform.Rotate(axis, 1);
-    }
+	public void FixedUpdate()
+	{
+		transform.Rotate(axis, 1);
+	}
 }
