4b7311abcbef1d64880f962b2df5a8fda4710fb5
uxtuno <uxtuno664@gmail.com>
2016-02-15 01:24:52 +0900

    ギミック起動時のアイコンを調整

--- Assets/Scripts/Main/Item/MiniMapIconBillboard.cs
+++ Assets/Scripts/Main/Item/MiniMapIconBillboard.cs
@@ -7,20 +7,25 @@ using System.Collections;
 public class MiniMapIconBillboard : MonoBehaviour {
 	private Camera targetCamera;
 	private Quaternion defaultRotation;
+	Vector3 toCamera;
 
 	void Start()
 	{
 		targetCamera = GameObject.FindGameObjectWithTag(Tags.MiniMapCamera).GetComponent<Camera>();
+		toCamera = targetCamera.transform.position - transform.position;
 		defaultRotation = transform.rotation;
 	}
 
 	void Update()
 	{
-		transform.forward = Vector3.up; ;
+		transform.forward = Vector3.up;
 	}
 
 	void LateUpdate()
 	{
-		transform.rotation = defaultRotation;
-	}
+		transform.forward = targetCamera.transform.forward;
+		Vector3 angles = transform.eulerAngles;
+		angles.y = targetCamera.transform.eulerAngles.y;
+		transform.eulerAngles = angles;
+    }
 }
