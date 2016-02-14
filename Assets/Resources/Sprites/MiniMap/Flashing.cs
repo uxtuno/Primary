72e5c3371eae00c556cebca587a2419fe0ecf1d1
uxtuno <uxtuno664@gmail.com>
2016-02-14 22:07:53 +0900

    ミニマップにギミックの起動を知らせるアイコンを追加

---
+++ Assets/Resources/Sprites/MiniMap/Flashing.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// オブジェクトを一定時間点滅後、破棄する
+/// </summary>
+public class Flashing : MyMonoBehaviour
+{
+	[SerializeField, Tooltip("生存時間")]
+	private float lifeSeconds = 0.0f;
+	[SerializeField, Tooltip("点滅間隔")]
+	private float flashingIntervalSeconds = 0.5f;
+
+	IEnumerator Start()
+	{
+		float count = 0.0f;
+		while (count < lifeSeconds)
+		{
+			yield return new WaitForSeconds(flashingIntervalSeconds);
+			IsShow = !IsShow;
+			count += flashingIntervalSeconds;
+		}
+		Destroy(gameObject);
+	}
+
+	void OnValidate()
+	{
+		if (lifeSeconds < 0.0f)
+			lifeSeconds = 0.0f;
+
+		if (flashingIntervalSeconds < 0.0f)
+			flashingIntervalSeconds = 0.0f;
+	}
+}
--- Assets/Scripts/Main/Gimmick/Gimmick.cs
+++ Assets/Scripts/Main/Gimmick/Gimmick.cs
@@ -15,5 +15,37 @@ public abstract class GimmickState
 /// </summary>
 public abstract class Gimmick : EventObject
 {
+	[SerializeField, Tooltip("ギミック起動時にアイコンを表示する位置")]
+	private Transform pickIconPosition;
+	private GameObject pickIconPrefab;
+	private GameObject pickIcon;
 
+	protected override void Awake()
+	{
+		base.Awake();
+		pickIconPrefab = Resources.Load<GameObject>("Prefabs/Gimmicks/PickIcon");
+	}
+
+	/// <summary>
+	/// ギミック起動時に呼ぶ
+	/// </summary>
+	/// <param name="pickIconPrefab"></param>
+	protected void ShowPickIcon()
+	{
+		ShowPickIcon(pickIconPosition.position);
+	}
+
+	/// <summary>
+	/// ギミック起動時に呼ぶ
+	/// </summary>
+	/// <param name="pickIconPrefab"></param>
+	protected void ShowPickIcon(Vector3 position)
+	{
+		if (pickIcon != null)
+		{
+			Destroy(pickIcon);
+		}
+
+		pickIcon = Instantiate(pickIconPrefab, position, Quaternion.identity) as GameObject;
+	}
 }
