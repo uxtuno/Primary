a461e5b4176f9d65909f08900b0fe2b1a1012775
Kuvo <Kuvo.Do.Laurant.Ehrengerg@gmail.com>
2016-02-19 02:56:07 +0900

    各種ステージの変更

---
+++ Assets/Scripts/NotMonoBehaviour/SortingLayerName.cs
@@ -0,0 +1,19 @@
+public class SortingLayerName
+{
+
+	/// <summary>
+	/// return "Default"
+ 	/// </summary>
+	public const string @Default = "Default";
+
+	/// <summary>
+	/// return "Effect"
+ 	/// </summary>
+	public const string @Effect = "Effect";
+
+	/// <summary>
+	/// <para>0. "Default"</para>
+	/// <para>1. "Effect"</para>
+	/// </summary>
+	public static readonly string[] names = new string[]{"Default","Effect"};
+}
