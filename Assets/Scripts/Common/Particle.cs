7a1f58e6de46cebd27db1ef696e235cd88802627
uxtuno <uxtuno664@gmail.com>
2016-05-12 02:25:58 +0900

    パーティクルのパフォーマンスを改善 生成時のカクツキが軽減された

--- Assets/Scripts/Common/Particle.cs
+++ Assets/Scripts/Common/Particle.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 using System.Linq;
 using System.Threading;
+using Object = UnityEngine.Object;
 
 public class Particle : MyMonoBehaviour
 {
@@ -9,8 +11,6 @@ public class Particle : MyMonoBehaviour
 
 	struct ParticleData
 	{
-		public Vector3 position;
-		public float size;
 		public Vector3 direction;
 		public float vy;
 		public float vy2;
@@ -20,22 +20,30 @@ public class Particle : MyMonoBehaviour
 
 	private ParticleSystem.Particle[] particles;
 	private ParticleData[] particleData = null;
-	private float distance = 2.0f;
+	private float distance;
 	private Thread particlePoitionCalculateThread = null;
-	private const int maxParticle = 200000;
+	private const int maxParticle = 2000;
 
-	void Start()
+	IEnumerator Start()
 	{
 		particleSystem = gameObject.GetComponent<ParticleSystem>();
+		distance = 1.0f;
+		isGenerated = false;
+		isInit = false;
+		isEnd = false;
+
+		new Thread(new ThreadStart(ParticleGenerate)).Start();
+		while (true)
+		{
+			yield return null;
+			if (isGenerated)
+				break;
+		}
+
 		particleSystem.maxParticles = maxParticle;
-		particles = new ParticleSystem.Particle[maxParticle];
-		particleData = new ParticleData[maxParticle];
 		particleSystem.Emit(maxParticle);
 		particleSystem.GetParticles(particles);
 
-		//new Thread(new ThreadStart(ParticleGenerate)).Start();
-
-		ParticleGenerate();
 		particlePoitionCalculateThread = new Thread(
 			new ThreadStart(ParticlePositionCalculate));
 
@@ -44,7 +52,19 @@ public class Particle : MyMonoBehaviour
 
 	private static readonly float oneFrameSeconds = 0.032f;
 
+	private bool isGenerated;
 	private void ParticleGenerate()
+	{
+		particles = new ParticleSystem.Particle[maxParticle];
+		particleData = new ParticleData[maxParticle];
+		lock (syncObj)
+		{
+			isGenerated = true;
+		}
+	}
+
+	private bool isInit;
+	private void ParticleInitialize()
 	{
 		System.Random r = new System.Random();
 		for (int i = 0; i < maxParticle; i++)
@@ -62,50 +82,61 @@ public class Particle : MyMonoBehaviour
 			particleData[i].direction = q * Vector3.forward;
 			particleData[i].speed = 1;
 			particleData[i].cos = i / ((float)maxParticle / 10);
-			particleData[i].position = particles[i].position;
-			particleData[i].size = particles[i].size;
+		}
+
+		lock (syncObj)
+		{
+			isInit = true;
 		}
 	}
 
 	private bool isEnd;
 	private void ParticlePositionCalculate()
 	{
-		isEnd = false;
+		ParticleInitialize();
+
 		while (true)
 		{
+			if (!isInit)
+			{
+				Thread.Sleep(0);
+				continue;
+			}
+
 			Vector3 newPosition = new Vector3();
 			for (int i = 0; i < particles.Length; i++)
 			{
+				Vector3 direction = particleData[i].direction;
+				float mul = (particleData[i].speed * (Mathf.Sin(particleData[i].cos))) * distance;
+
+				direction.x *= mul;
+				direction.z *= mul;
+				direction.y *= mul * 5.0f;
+				newPosition.x = direction.x;
+				newPosition.y = direction.y + particleData[i].vy2;
+				newPosition.z = direction.z;
+
+				particleData[i].cos += oneFrameSeconds / 10.0f;
+				particleData[i].vy += oneFrameSeconds / 20.0f;
+				particleData[i].vy2 += particleData[i].vy;
+
+				// スレッドの外で参照されるのはここだけ
 				lock (syncObj)
 				{
-					Vector3 direction = particleData[i].direction;
-					float mul = (particleData[i].speed * (Mathf.Sin(particleData[i].cos)));
+					particles[i].position = newPosition;
+					particles[i].size -= 0.08f * oneFrameSeconds;
 
-					direction.x *= mul * distance;
-					direction.z *= mul * distance;
-					direction.y *= mul * distance * 5.0f;
-					newPosition.x = direction.x;
-					newPosition.y = direction.y + particleData[i].vy2;
-					newPosition.z = direction.z;
-
-					particleData[i].cos += oneFrameSeconds / 10.0f;
-					particleData[i].vy += oneFrameSeconds / 20.0f;
-					particleData[i].vy2 += particleData[i].vy;
-					particleData[i].position = newPosition;
-					particleData[i].size = particleData[i].size - 0.08f * oneFrameSeconds;
-					particles[i].position = particleData[i].position;
-					particles[i].size = particleData[i].size;
-					if (particleData[i].size < 0.0f)
+					if (particles[i].size < 0.0f)
 					{
-						particleData[i].size = 0.0f;
+						particles[i].size = 0.0f;
+						isEnd = true;
 						break;
 					}
 				}
+
 			}
 
-			if (particleData[0].size <= 0.0f)
-				isEnd = true;
-
+			distance *= 1.04f;
 			Thread.Sleep(Mathf.FloorToInt(1000 * oneFrameSeconds));
 		}
 	}
@@ -117,6 +148,9 @@ public class Particle : MyMonoBehaviour
 
 	protected override void Update()
 	{
+		if (!isInit)
+			return;
+
 		Vector3 newPosition = new Vector3();
 
 		if (isEnd)
@@ -126,7 +160,6 @@ public class Particle : MyMonoBehaviour
 		}
 
 		particleSystem.SetParticles(particles, particles.Length);
-
 		transform.Rotate(0.0f, 120f * Time.deltaTime, 0.0f);
 	}
 
